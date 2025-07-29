b6ca65760d20335d93d54278832678050de56159
lahari-k-2005 <k.laharireddy2005@gmail.com>
2025-07-29 17:14:24 +0530

    Resolved merge conflicts

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+        static_folder='static',
+        template_folder='templates')
 
     from .routes.dashboard_routes import dashboard_bp
 
