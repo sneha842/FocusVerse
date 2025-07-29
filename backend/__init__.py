f10165a71efe136a17599e0bf70fc6b3bfe3339c
Sneha Srivastava <srivastavasneha895@gmail.com>
2025-07-29 17:31:24 +0530

    Added

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,7 @@
-import os
 from flask import Flask
 
 def create_app():
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-
-    app = Flask(
-        __name__,
-        static_folder=os.path.join(base_dir, 'static'),
-        template_folder=os.path.join(base_dir, 'templates')
-    )
+    app = Flask(_name_)  # don't set paths here
 
     from .routes.dashboard_routes import dashboard_bp
     app.register_blueprint(dashboard_bp)
